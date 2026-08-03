99176e781fc98849c78f484ddbd641cf8ae9d907
XCR-hub <tcerda@xcr.fr>
2026-08-03 17:19:11 +0200

    Publish static Jeanbrun build (b9fd4d1)

---
+++ assets/siteIcons-BvpNmdyP.js
@@ -0,0 +1,16 @@
+import{B as t}from"./building-2-Blz362H1.js";import{S as c}from"./shield-check-CqQKrl-8.js";import{S as a}from"./shield-CsEA02g5.js";import{L as s}from"./landmark-ZDBPeNe0.js";import{c as e}from"./index-bIgQbiz5.js";import{H as m}from"./home-DShmpem4.js";import{G as i}from"./globe-CNReLJC6.js";import{C as n}from"./calculator-CwsvB2TP.js";/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=e("Gem",[["path",{d:"M6 3h12l4 6-10 13L2 9Z",key:"1pcd5k"}],["path",{d:"M11 3 8 9l4 13 4-13-3-6",key:"1fcu3u"}],["path",{d:"M2 9h20",key:"16fsjt"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=e("HeartPulse",[["path",{d:"M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z",key:"c3ymky"}],["path",{d:"M3.22 12H9.5l.5-1 2 4.5 2-7 1.5 3.5h5.27",key:"1uw2ng"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=e("KeyRound",[["path",{d:"M2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4a6.5 6.5 0 1 0-4-4Z",key:"167ctg"}],["circle",{cx:"16.5",cy:"7.5",r:".5",fill:"currentColor",key:"w0ekpg"}]]),r={Building2:t,Calculator:n,Gem:p,Globe:i,HeartPulse:l,Home:m,KeyRound:h,Landmark:s,Shield:a,ShieldCheck:c},S=Object.keys(r).sort();function C(o){return o?r[o]??t:t}export{C as g,S as s};
