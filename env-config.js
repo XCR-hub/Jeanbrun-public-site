7a7d28bb924bec1dcd5b64d8955a7a0ef7249cf2
XCR-hub <tcerda@xcr.fr>
2026-08-03 07:30:00 +0200

    Refresh API tunnel origin

--- env-config.js
+++ env-config.js
@@ -1,1 +1,1 @@
-window.__APP_CONFIG__={"VITE_JEANBRUN_API_URL":"https://horse-varieties-cake-plug.trycloudflare.com","VITE_TURNSTILE_SITE_KEY":""};
+window.__APP_CONFIG__={"VITE_JEANBRUN_API_URL":"https://estimation-ebony-vanilla-heat.trycloudflare.com","VITE_TURNSTILE_SITE_KEY":""};
