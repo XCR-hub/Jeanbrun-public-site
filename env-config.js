dafa48d01d6e806abab5e503988ea4bd23c79455
XCR-hub <tcerda@xcr.fr>
2026-08-03 07:25:14 +0200

    Point runtime API to active selfhost tunnel

--- env-config.js
+++ env-config.js
@@ -1,1 +1,1 @@
-window.__APP_CONFIG__={"VITE_JEANBRUN_API_URL":"https://api.loi-jeanbrun-invest.com","VITE_TURNSTILE_SITE_KEY":""};
+window.__APP_CONFIG__={"VITE_JEANBRUN_API_URL":"https://evident-fragrance-existed-refers.trycloudflare.com","VITE_TURNSTILE_SITE_KEY":""};
