aacdec9c3036ced4dadbb220ef79fa4f604fa809
XCR-hub <tcerda@xcr.fr>
2026-08-03 07:57:31 +0200

    Publish resilient API fallback build

--- env-config.js
+++ env-config.js
@@ -1,1 +1,1 @@
-window.__APP_CONFIG__={"VITE_JEANBRUN_API_URL":"https://experiment-string-legs-bytes.trycloudflare.com","VITE_TURNSTILE_SITE_KEY":""};
+window.__APP_CONFIG__=window.__APP_CONFIG__||{};
